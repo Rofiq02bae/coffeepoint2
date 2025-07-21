910a86ef73ffccb323503ff24d56b33435bb901c
rofiq02bae <145300940+Rofiq02bae@users.noreply.github.com>
2025-07-21 13:03:14 +0000

    Add .gitignore file to exclude environment and build artifacts

--- app/firebase.py
+++ app/firebase.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 import os
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase-credentials.json")
+    cred = credentials.Certificate("coffeepoint2.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
