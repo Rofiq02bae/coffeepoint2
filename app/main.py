0ed0db48279d15f505431a31bb588527215506ed
rofiq02bae <145300940+Rofiq02bae@users.noreply.github.com>
2025-07-21 23:51:29 +0000

    Update Procfile and main.py for application configuration and routing

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
+# from fastapi import FastAPI
+# from routes import transaction
+
+# app = FastAPI(title="Coffeepoint API")
+
+# app.include_router(transaction.router, prefix="/transaction", tags=["Transaction"])
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Coffeepoint Backend is running!"}
 from fastapi import FastAPI
-from routes import transaction
 
-app = FastAPI(title="Coffeepoint API")
-
-app.include_router(transaction.router, prefix="/transaction", tags=["Transaction"])
+app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Coffeepoint Backend is running!"}
+    return {"message": "Hello from Coffeepoint API!"}
